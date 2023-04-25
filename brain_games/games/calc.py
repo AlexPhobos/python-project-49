230c4c34f0112edf6dd26e89762f0cce53ce5a66
Alexander Sheremetev <kriventsoff2011@gmail.com>
2023-04-25 20:54:03 +0300

    Calc game done

---
+++ brain_games/games/calc.py
@@ -0,0 +1,15 @@
+from random import randint, choice
+
+
+Rule = 'What is the result of the expression?'
+def game():
+    number_1, number_2 = randint(1, 100), randint(1, 100)
+    operation = choice('+-*')
+    number = f'{number_1} {operation} {number_2}'
+    if operation  == '+':
+        correct_answer = str(number_1 + number_2)
+    elif operation  == '-':
+        correct_answer = str(number_1 - number_2)
+    elif operation  == '*':
+        correct_answer = str(number_1 * number_2)
+    return number, correct_answer
---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,10 @@
+from brain_games.logic import start
+from brain_games.games import calc
+
+
+def main():
+    start(calc)
+
+
+if __name__ == '__main__':
+    main()

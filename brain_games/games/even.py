e758270732c00114a16a578ae294ada4a2709088
Alexander Sheremetev <kriventsoff2011@gmail.com>
2023-04-25 18:12:01 +0300

    Even game

---
+++ brain_games/games/even.py
@@ -0,0 +1,11 @@
+from random import randint
+
+
+Rule = 'Answer "yes" if the number is even, otherwise answer "no".'
+def game():
+    number = randint(1, 100)
+    if number % 2 == 0:
+        correct_answer = 'yes'
+    else:
+        correct_answer = 'no'
+    return number, correct_answer
---
+++ brain_games/logic.py
@@ -0,0 +1,21 @@
+import prompt
+Levels = 3
+
+
+def start(act):
+    print("Welcome to the Brain Games!")
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print(act.Rule)
+    for _ in range(Levels):
+        number, correct_answer = act.game()
+        print(f'Question: {number}')
+        answer = prompt.string('Your answer: ')
+        if answer == correct_answer:
+            print('Correct!')
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.\n"
+            f"Let's try again, {name}!")
+            break
+    else:
+        print(f'Congratulations, {name}!')
